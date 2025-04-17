916a6929706af4c22de77b853662e5e29e49f1f5
Raghavg18 <graghav2006@gmail.com>
2025-04-18 02:26:34 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -60,31 +60,6 @@ export default function NotebookLM() {
             </Button>
 
             <div className="flex items-center">
-              <div className="flex border rounded-l-md overflow-hidden">
-                <Button variant="ghost" className="rounded-none border-r h-9 px-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="text-blue-600"
-                  >
-                    <polyline points="9 11 12 14 22 4"></polyline>
-                    <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
-                  </svg>
-                </Button>
-                <Button variant="ghost" className="rounded-none border-r h-9 px-3">
-                  <Grid className="w-5 h-5" />
-                </Button>
-                <Button variant="ghost" className="rounded-none h-9 px-3">
-                  <List className="w-5 h-5" />
-                </Button>
-              </div>
 
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
